46df67100ff2de6081932a88c746b5f21e107061
Himanshu Kushwaha <86154077+Himanshhhyou@users.noreply.github.com>
2023-09-09 08:29:01 +0000

    del files

--- reverseNum.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter any number: "))
-tempNum = num
-reverseNum = 0
-while tempNum!=0:
-	lastDigit = tempNum%10
-	reverseNum =reverseNum*10+lastDigit
-	tempNum//=10
-
-print("Reverse of {} is {}".format(num,reverseNum))
