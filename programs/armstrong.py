1a8e089b2152db7d1c18d01e5d52e11bd66560fe
Himanshu Kushwaha <86154077+Himanshhhyou@users.noreply.github.com>
2024-02-23 21:31:57 +0530

    update

--- programs/armstrong.py
+++ programs/armstrong.py
@@ -1,11 +1,11 @@
-num = input("Enter the number: ")
+num = int(input("Enter the number: "))
 temp = num
 total = 0
-while num > 0:
+while temp > 0:
     lastDigit = temp % 10
     total = total + (lastDigit ** len(str(num)))
     temp = temp // 10
 if num == total:
-    print(str(num)+" is an armstrong number"))
+    print(str(num)+" is an armstrong number")
 else:
-    print(str(num)+" isn't an armstrong number"))
+    print(str(num)+" isn't an armstrong number")
